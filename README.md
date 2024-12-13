e92aae7b5d391c28195dcbff262ee14af409b6d6
JuanDaDiaz1005 <juan.1701920068@ucaldas.edu.co>
2024-12-12 22:13:35 -0500

    Se crea el main y las pruebas correspondientes, además se añade el readme.md y el archivo de requerimientos

---
+++ README.md
@@ -0,0 +1,3 @@
+# DevOps Reto
+
+Este es un proyecto básico de Python con un flujo CI configurado en GitHub Actions.
---
+++ app/main.py
@@ -0,0 +1,14 @@
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def main():
+    suma = add(1, 2)
+    resta = subtract(3, 2)
+    print(suma)
+    print(resta)
+
+if __name__ == "__main__":
+    main()
